38e69fa0c124af49ecccb7b179e1db4150a95af6
a-box31 <thomaa2031@gmail.com>
2023-05-13 03:47:57 -0400

    Made the Nav and Header

--- nav.js
+++ nav.js
@@ -8,7 +8,7 @@ const wrapper = document.getElementsByClassName('wrapper')[0]
 
 wrapper.onscroll = function(){
         if( wrapper.scrollTop > 50 ){
-            nav.style.backgroundColor = 'black'
+            nav.style.backgroundColor = 'rgba(0, 0, 0, 0.93)'
         }else{
             nav.style.backgroundColor = 'transparent'
         }
